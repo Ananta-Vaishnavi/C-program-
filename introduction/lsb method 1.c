239984d70677d1c935bdfd79bfdb5ef64fba5b24
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-01-15 12:25:34 +0530

    Add files via upload

---
+++ introduction/lsb method 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+//lsb of a number without bitwise operators
+ int a;
+ printf("enter an integer");
+ scanf("%d",&a);
+    printf("The lsb of the no. is %d", a%10);  
+ 
+}
